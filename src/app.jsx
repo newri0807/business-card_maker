53981cf15203f0d1c9dd85631e8f4e10b65e8b78
newls <newri0807@naver.com>
2021-01-17 13:02:20 +0900

    first set

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styles from './app.module.css';
+import Login from './components/login/login';
+import Maker from './components/maker/maker';
 
-import './app.css';
-
-function App() {
+function App({ FileInput, authService, cardRepository, index }) {
   return (
-    <h1>hello~ </h1>
+    <div className={styles.app}>
+      <BrowserRouter basename={window.location.pathname || ''}>
+        <Switch>
+          <Route exact path="/" component={index}>
+            <Login authService={authService} />
+          </Route>
+          <Route path="/maker">
+            <Maker
+              FileInput={FileInput}
+              authService={authService}
+              cardRepository={cardRepository}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
