ad5a7a5418d319223711cc6ecddae97334e7afce
newls <newri0807@naver.com>
2021-01-17 13:53:31 +0900

    Updates

---
+++ precache-manifest.398623a534b9d1c4904b1a5aa0382914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f667d47a98697838e536c219f92d042d",
+    "url": "/business-card_maker/index.html"
+  },
+  {
+    "revision": "1f61956d1f927fb58ee4",
+    "url": "/business-card_maker/static/css/main.63471f36.chunk.css"
+  },
+  {
+    "revision": "6b8ee24297b25f699d70",
+    "url": "/business-card_maker/static/js/2.e518df26.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/business-card_maker/static/js/2.e518df26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f61956d1f927fb58ee4",
+    "url": "/business-card_maker/static/js/main.1c401a0f.chunk.js"
+  },
+  {
+    "revision": "cbfa1383b4abe6eefe7b",
+    "url": "/business-card_maker/static/js/runtime-main.23bfd53f.js"
+  }
+]);
